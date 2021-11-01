542ac5a35c055dcdb63182b747eb16a153117ccf
KaitlynSs <katie.spehn@smoothstack.com>
2021-11-01 09:57:55 -0500

    database schemas

---
+++ Database schemas/camping-community-schema.sql
@@ -0,0 +1,12 @@
+drop database if exists camping_community;
+create database camping_community;
+use camping_community;
+
+-- create tables and relationships
+create table tableName (
+    
+);
+
+-- data
+insert into tableName values
+	();
---
+++ Database schemas/camping-community-test-schema.sql
@@ -0,0 +1,12 @@
+drop database if exists camping_community_test;
+create database camping_community_test;
+use camping_community_test;
+
+-- create tables and relationships
+create table tableName (
+    
+);
+
+-- data
+insert into tableName values
+	();
