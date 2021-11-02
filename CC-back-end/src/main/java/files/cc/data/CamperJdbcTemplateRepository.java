b181b2eb57b6a3184d5d006d70d05516e07dd3be
t-nikolai <thomas.nikolai@marquette.edu>
2021-11-01 19:58:18 -0500

    backend: updates to camper files

--- CC-back-end/src/main/java/files/cc/data/CamperJdbcTemplateRepository.java
+++ CC-back-end/src/main/java/files/cc/data/CamperJdbcTemplateRepository.java
@@ -1,5 +1,6 @@
 package files.cc.data;
 
+import files.cc.data.mappers.CamperMapper;
 import files.cc.models.Camper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,11 +13,27 @@ public class CamperJdbcTemplateRepository {
 
     public CamperJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
 
-public List<Camper> findAll() {
-    final String sql = "select camper_id, first_name, last_name, camping_method, camper_phone, camper_email, " +
+    public List<Camper> findAll() {
+        final String sql = "select camper_id, first_name, last_name, camping_method, camper_phone, camper_email, " +
             "camper_address, camper_city, camper_state, camper_zip " +
             "from camper limit 1000; ";
-    return jdbcTemplate.query(sql.);  //TODO: unfinished line of code
-}
+        return jdbcTemplate.query(sql, new CamperMapper());
+    }
+
+    public Camper findById(int camper_id){
+        final String sql = "select camper_id, first_name, last_name, camping_method, camper_phone, " +
+                "camper_email, camper_address, camper_city, camper_state, camper_zip " +
+                "from camper " +
+                "where camper_id = ? ;";
+        Camper camper = jdbcTemplate.query(sql, new CamperMapper(), camper_id).stream()
+                .findFirst().orElse(null);
+
+//        if (camper != null) {
+//            addAgencies(agent);
+//            addAliases(agent);
+//        }
+
+        return camper;
+    }
 
 }
--- CC-back-end/src/main/java/files/cc/data/mappers/CamperMapper.java
+++ CC-back-end/src/main/java/files/cc/data/mappers/CamperMapper.java
@@ -1,4 +1,25 @@
 package files.cc.data.mappers;
 
-public class CamperMapper {
+import files.cc.models.Camper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+public class CamperMapper implements RowMapper<Camper> {
+    //@Override
+    public Camper mapRow(ResultSet resultSet, int i) throws SQLException {
+        Camper camper = new Camper();
+        camper.setCamper_id(resultSet.getInt("camper_id"));
+        camper.setFirst_name(resultSet.getString("first_name"));
+        camper.setLast_name(resultSet.getString("last_name"));
+        camper.setCamper_address(resultSet.getString("address"));
+        camper.setCamper_city(resultSet.getString("city"));
+        camper.setCamper_state(resultSet.getString("state"));
+        camper.setCamper_zip(resultSet.getInt("zip_code"));
+        camper.setCamper_email(resultSet.getString("email"));
+        camper.setCamper_phone(resultSet.getString("phone"));
+
+        return camper;
+    }
 }
