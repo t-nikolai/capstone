d9e7edcdfdf86c3d96b57faef9b151a3b93a2c49
Rowan Meyers <rmeyers@dev-10.com>
2021-11-02 10:22:35 -0500

    fixing merge conflicts in CamperRepo

--- CC-back-end/src/main/java/files/cc/data/CamperJdbcTemplateRepository.java
+++ CC-back-end/src/main/java/files/cc/data/CamperJdbcTemplateRepository.java
@@ -19,29 +19,17 @@ public class CamperJdbcTemplateRepository implements CamperRepository {
     public CamperJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
 
     @Override
-<<<<<<< HEAD
-    public List<Camper> findAll() {
-        final String sql = "select camper_id, first_name, last_name, camping_method, phone, email, " +
-            "address, city, state, zip " +
-=======
     public List<Camper> findAll() {     // ADMIN Privileges
         final String sql = "select camper_id, first_name, last_name, camping_method, camper_phone, camper_email, " +
             "camper_address, camper_city, camper_state, camper_zip " +
->>>>>>> 98c37b47df56814d33ee66f3db6e30ae6c10b5dc
             "from camper limit 1000; ";
         return jdbcTemplate.query(sql, new CamperMapper());
     }
 
     @Override
-<<<<<<< HEAD
-    public Camper findById(int camper_id){
-        final String sql = "select camper_id, first_name, last_name, method, phone, " +
-                "email, address, city, state, zip " +
-=======
     public Camper findById(int camper_id){      // ADMIN Privileges
         final String sql = "select camper_id, first_name, last_name, camping_method, camper_phone, " +
                 "camper_email, camper_address, camper_city, camper_state, camper_zip " +
->>>>>>> 98c37b47df56814d33ee66f3db6e30ae6c10b5dc
                 "from camper " +
                 "where camper_id = ? ;";
         Camper camper = jdbcTemplate.query(sql, new CamperMapper(), camper_id).stream()
@@ -65,15 +53,15 @@ public class CamperJdbcTemplateRepository implements CamperRepository {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         int rowsAffected = jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, camper.getFirst_name());
-            ps.setString(2, camper.getLast_name());
-            ps.setString(3, camper.getCamping_method());
-            ps.setString(4, camper.getCamper_phone());
-            ps.setString(5, camper.getCamper_email());
-            ps.setString(6, camper.getCamper_address());
-            ps.setString(7, camper.getCamper_city());
-            ps.setString(8, camper.getCamper_state());
-            ps.setInt(9, camper.getCamper_zip());
+            ps.setString(1, camper.getFirstName());
+            ps.setString(2, camper.getLastName());
+            ps.setString(3, camper.getCampingMethod());
+            ps.setString(4, camper.getPhone());
+            ps.setString(5, camper.getEmail());
+            ps.setString(6, camper.getAddress());
+            ps.setString(7, camper.getCity());
+            ps.setString(8, camper.getState());
+            ps.setInt(9, camper.getZip());
             return ps;
         }, keyHolder);
 
@@ -81,7 +69,7 @@ public class CamperJdbcTemplateRepository implements CamperRepository {
             return null;
         }
 
-        camper.setCamper_id(keyHolder.getKey().intValue());
+        camper.setCamperId(keyHolder.getKey().intValue());
         return camper;
     }
 
@@ -99,16 +87,16 @@ public class CamperJdbcTemplateRepository implements CamperRepository {
                     + "zip = ? ;";
 
             return jdbcTemplate.update(sql,
-                    camper.getFirst_name(),
-                    camper.getLast_name(),
-                    camper.getCamping_method(),
-                    camper.getCamper_phone(),
-                    camper.getCamper_email(),
-                    camper.getCamper_address(),
-                    camper.getCamper_city(),
-                    camper.getCamper_state(),
-                    camper.getCamper_zip(),
-                    camper.getCamper_id()) > 0;
+                    camper.getFirstName(),
+                    camper.getLastName(),
+                    camper.getCampingMethod(),
+                    camper.getPhone(),
+                    camper.getEmail(),
+                    camper.getAddress(),
+                    camper.getCity(),
+                    camper.getState(),
+                    camper.getZip(),
+                    camper.getCamperId()) > 0;
     }
 
     @Override
