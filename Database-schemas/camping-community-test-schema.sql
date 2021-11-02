fdc3f65b02823fecf1c1919e300562c2d3405e3b
t-nikolai <thomas.nikolai@marquette.edu>
2021-11-02 15:20:55 -0500

    more schema changes

--- Database-schemas/camping-community-test-schema.sql
+++ Database-schemas/camping-community-test-schema.sql
@@ -61,8 +61,8 @@ create table reservation (
 );
 
 -- data
-insert into campground(campground_name, campground_address, campground_city, campground_state, campground_zip,
-	campground_phone, campground_email, campground_capacity, standard_rate, weekend_rate)
+insert into campground(`name`, address, city, state, zip,
+	phone, email, capacity, standard_rate, weekend_rate)
 values("Test CG Name 1", "CG Street 1", "CG City 1", "S1", 11111, "(111)111-1111", 
 	"cgemail1@cgemail1.com", 11, 10.00, 11.00);
     
@@ -80,7 +80,7 @@ insert into campsite(site_name, campground_id)
     ("Site 10", 1),
     ("Site 11", 1);
     
-insert into camper(first_name, last_name, camping_method, camper_phone, camper_email, camper_address, camper_city, camper_state, camper_zip)
+insert into camper(first_name, last_name, camping_method, phone, email, address, city, state, zip)
 	values
     ("C First Name 1", "C Last Name 1", "C Method 1", "(111)111-1111", "cemail1@cemail1.com", "C Street 1", "C City 1", "S1", 11111);
     
@@ -101,10 +101,17 @@ alter table camper auto_increment = 1;
 delete from reservation;
 alter table reservation auto_increment = 1;
 
-insert into campground(campground_name, campground_address, campground_city, campground_state, campground_zip,
-	campground_phone, campground_email, campground_capacity, standard_rate, weekend_rate)
+insert into campground(`name`, address, city, state, zip,
+	phone, email, capacity, standard_rate, weekend_rate)
 values("Test CG Name 1", "CG Street 1", "CG City 1", "S1", 11111, "(111)111-1111", 
-	"cgemail1@cgemail1.com", 11, 10.00, 11.00);
+	"cgemail1@cgemail1.com", 11, 10.00, 11.00),
+    ("Test CG Name 2", "CG Street 2", "CG City 2", "S2", 22222, "(222)222-2222", 
+	"cgemail2@cgemail2.com", 22, 20.00, 22.00),
+    ("Test CG Name 3", "CG Street 3", "CG City 3", "S3", 33333, "(333)333-3333", 
+	"cgemail3@cgemail3.com", 33, 30.00, 33.00),
+    ("Test CG Name 4", "CG Street 4", "CG City 4", "S4", 44444, "(444)444-4444", 
+	"cgemail4@cgemail4.com", 44, 14.00, 44.00);
+	
     
 insert into campsite(site_name, campground_id)
 	values
@@ -118,9 +125,45 @@ insert into campsite(site_name, campground_id)
     ("Site 8", 1),
     ("Site 9", 1),
     ("Site 10", 1),
-    ("Site 11", 1);
+    ("Site 11", 1), 
+    -- cg 2
+    ("Site 1", 2),
+    ("Site 2", 2),
+    ("Site 3", 2),
+    ("Site 4", 2),
+    ("Site 5", 2),
+    ("Site 6", 2),
+    ("Site 7", 2),
+    ("Site 8", 2),
+    ("Site 9", 2),
+    ("Site 10", 2),
+    ("Site 11", 2),
+    -- cg 3
+    ("Site 1", 3),
+    ("Site 2", 3),
+    ("Site 3", 3),
+    ("Site 4", 3),
+    ("Site 5", 3),
+    ("Site 6", 3),
+    ("Site 7", 3),
+    ("Site 8", 3),
+    ("Site 9", 3),
+    ("Site 10", 3),
+    ("Site 11", 3),
+    -- cg 4
+    ("Site 1", 4),
+    ("Site 2", 4),
+    ("Site 3", 4),
+    ("Site 4", 4),
+    ("Site 5", 4),
+    ("Site 6", 4),
+    ("Site 7", 4),
+    ("Site 8", 4),
+    ("Site 9", 4),
+    ("Site 10", 4),
+    ("Site 11", 4);
     
-insert into camper(first_name, last_name, camping_method, camper_phone, camper_email, camper_address, camper_city, camper_state, camper_zip)
+insert into camper(first_name, last_name, camping_method, phone, email, address, city, state, zip)
 	values
     ("C First Name 1", "C Last Name 1", "C Method 1", "(111)111-1111", "cemail1@cemail1.com", "C Street 1", "C City 1", "S1", 11111);
     
