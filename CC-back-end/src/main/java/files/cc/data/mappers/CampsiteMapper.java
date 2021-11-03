23102951d9e573e86b77d583ae5abfecf474786d
Rowan Meyers <rmeyers@dev-10.com>
2021-11-03 11:50:50 -0500

    working on campsite repository

--- CC-back-end/src/main/java/files/cc/data/mappers/CampsiteMapper.java
+++ CC-back-end/src/main/java/files/cc/data/mappers/CampsiteMapper.java
@@ -1,4 +1,22 @@
 package files.cc.data.mappers;
 
-public class CampsiteMapper {
+import files.cc.models.Campground;
+import files.cc.models.Campsite;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CampsiteMapper implements RowMapper<Campsite> {
+    @Override
+    public Campsite mapRow(ResultSet resultSet, int i) throws SQLException {
+        Campsite campsite = new Campsite();
+        campsite.setSiteId(resultSet.getInt("site_id"));
+        campsite.setName(resultSet.getString("name"));
+
+        CampgroundMapper campgroundMapper = new CampgroundMapper();
+        campsite.setCampground(campgroundMapper.mapRow(resultSet, i));
+
+        return campsite;
+    }
 }
