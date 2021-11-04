bebd2b3ffc09fbf9d8dd8990b4a4c8c89ccce501
t-nikolai <thomas.nikolai@marquette.edu>
2021-11-03 20:14:51 -0500

    commit

--- CC-back-end/src/test/java/files/cc/domain/CamperServiceTest.java
+++ CC-back-end/src/test/java/files/cc/domain/CamperServiceTest.java
@@ -1,12 +1,15 @@
 package files.cc.domain;
 
 import files.cc.data.CamperRepository;
+import files.cc.models.Camper;
+import files.cc.models.Campground;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class CamperServiceTest {
@@ -36,16 +39,37 @@ class CamperServiceTest {
 //  ADD TESTS
     @Test
     void shouldAdd(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
 
+        when(repository.add(camper)).thenReturn(camper);
+        result=service.add(camper);
+        assertEquals(ResultType.SUCCESS,result.getType());
     }
 
     @Test
     void shouldNotAddIfNull(){
+        Camper emptyCamper = new Camper();
 
+        Result<Camper> result = service.add(null);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        result = service.add(emptyCamper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
     void shouldNotAddIfNameIsInvalid(){
+        Camper camper = new Camper();
+        Result<Camper> result;
+        camper.setFirstName(null);
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        camper.setFirstName("Doug");
+        camper.setLastName("");
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
 
     }
 
@@ -95,5 +119,20 @@ class CamperServiceTest {
     @Test
     void shouldNotDeleteIfInvalid(){
 
+    }
+
+    private Camper makeCamper(){
+        Camper camper = new Camper();
+        camper.setFirstName("Doug");
+        camper.setLastName("Dimmadome");
+        camper.setPhone("(777)222-9101");
+        camper.setEmail("DD@fishersunited.com");
+        camper.setCampingMethod("Tent");
+        camper.setAddress("123 Western Heights");
+        camper.setCity("Portland");
+        camper.setState("IL");
+        camper.setZip(60053);
+
+        return camper;
     }
 }
