3b5a4686026b9cf48e6d91474ad385c70245fac6
t-nikolai <thomas.nikolai@marquette.edu>
2021-11-03 21:23:38 -0500

    camper service tests - mostly completed

--- CC-back-end/src/test/java/files/cc/domain/CamperServiceTest.java
+++ CC-back-end/src/test/java/files/cc/domain/CamperServiceTest.java
@@ -2,7 +2,7 @@ package files.cc.domain;
 
 import files.cc.data.CamperRepository;
 import files.cc.models.Camper;
-import files.cc.models.Campground;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -75,38 +75,94 @@ class CamperServiceTest {
 
     @Test
     void shouldNotAddIfAddressIsInvalid(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
+        camper.setAddress(null);
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
+        
+        camper =makeCamper();
+        camper.setCity(null);
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
 
+        camper =makeCamper();
+        camper.setState("ABCD");
+        result = service.add(camper);
+        assertEquals(ResultType.FAIL, result.getType()); // fails, not invalid
+        
+        camper = makeCamper();
+        camper.setZip(888888);
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
     void shouldNotAddInvalidEmail(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
 
+        camper.setEmail(null);
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        camper.setEmail("hereIsAnEmailAddress.com");
+        result = service.add(camper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
 //  UPDATE TESTS
     @Test
     void shouldUpdate(){
-
+        Camper camper = makeCamper();
+        Result<Camper> result;
+        when(repository.update(camper)).thenReturn(true);
+        
+        camper.setCamperId(1);
+        result=service.update(camper);
+        assertEquals(ResultType.SUCCESS,result.getType());
     }
 
     @Test
     void shouldNotUpdateIfNull(){
+        Camper emptyCamper = new Camper();
+        Result<Camper> result;
 
+        result = service.update(null);
+        assertEquals(ResultType.INVALID, result.getType());
+
+        result = service.update(emptyCamper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
     void shouldNotUpdateIfNameIsInvalid(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
 
+        camper.setFirstName("Billiam");
+        result = service.update(camper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
     void shouldNotUpdateIfAddressIsInvalid(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
 
+        camper.setAddress(null);
+        result = service.update(camper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
     @Test
     void shouldNotUpdateIfEmailIsInvalid(){
+        Camper camper = makeCamper();
+        Result<Camper> result;
 
+        camper.setAddress("Billy.com");
+        result = service.update(camper);
+        assertEquals(ResultType.INVALID, result.getType());
     }
 
 //  DELETE TESTS
