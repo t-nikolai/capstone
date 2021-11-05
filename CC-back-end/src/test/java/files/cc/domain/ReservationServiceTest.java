fa1544f9bd18cce2d01bf2d610b03cf649163835
Rowan Meyers <rmeyers@dev-10.com>
2021-11-05 09:45:28 -0500

    committing start of service for other updates to be pulled

--- CC-back-end/src/test/java/files/cc/domain/ReservationServiceTest.java
+++ CC-back-end/src/test/java/files/cc/domain/ReservationServiceTest.java
@@ -1,7 +1,32 @@
 package files.cc.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import files.cc.data.ReservationRepository;
+import files.cc.models.Reservation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ReservationServiceTest {
 
+    @Autowired
+    ReservationService service;
+
+    @MockBean
+    ReservationRepository repository;
+
+    @Test
+    void shouldFindAll(){
+        List<Reservation> mockReservations = List.of(new Reservation(), new Reservation(), new Reservation(), new Reservation());
+
+        when(repository.findAll()).thenReturn(mockReservations);
+
+        
+    }
 }
